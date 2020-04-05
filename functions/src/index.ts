fc4ac090b3432dc0f17d6877d4749131ee9b0528
francesco <francesco@camberi.com>
2020-04-05 20:43:03 +0200

    Updated receiver function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,7 @@ import * as admin from "firebase-admin";
 // });
 
 
+type DocumentReference = admin.firestore.DocumentReference;
 export const on_receiver_update = functions
     .region('europe-west1')
     .firestore
@@ -19,60 +20,28 @@ export const on_receiver_update = functions
         if (!change.after.exists)
             return Promise.resolve("Deleted");
 
-        const contributorRefAfter: admin.firestore.DocumentReference = change.after.get("contributor_match");
-        const contributorRefBefore: admin.firestore.DocumentReference = change.before.get("contributor_match");
-        if (contributorRefAfter !== contributorRefBefore) {
-            contributorRefAfter.update("receiver_match", change.after.ref).catch(console.error);
-            console.log("Added receiver match to", contributorRefAfter);
-        }
+        const shouldUpdateStatus = !change.before.exists || change.before.get("status") !== change.after.get("status");
 
-        // const donorsRefsAfter: admin.firestore.DocumentReference[] = change.after.get("donor_matches");
-        // const donorsRefsBefore: admin.firestore.DocumentReference[] = change.before.get("donor_matches");
-        // if (donorsRefsAfter !== donorsRefsBefore) {
-        //     contributorRefAfter.update("receiver_match", change.after.ref).catch(console.error);
-        //     console.log("Added receiver match to", contributorRefAfter);
-        // }
-
-        return Promise.resolve("Updated references");
-    });
-
-export const on_contributor_update = functions
-    .region('europe-west1')
-    .firestore
-    .document('/contributor/{userId}')
-    .onWrite((change, context) => {
-
-        // if the entry is deleted, we stop
-        if (!change.after.exists)
-            return Promise.resolve("Deleted");
-
-        const receiverRefAfter: admin.firestore.DocumentReference = change.after.get("receiver_match");
-        const receiverRefBefore: admin.firestore.DocumentReference = change.before.get("receiver_match");
-        if (receiverRefAfter !== receiverRefBefore) {
-            receiverRefAfter.update("contributor_match", change.after.ref).catch(console.error);
-            console.log("Added contributor match to", receiverRefAfter);
-        }
-
-        return Promise.resolve("Updated references");
-    });
-
-export const on_donor_update = functions
-    .region('europe-west1')
-    .firestore
-    .document('/donor/{userId}')
-    .onWrite((change, context) => {
-
-        // if the entry is deleted, we stop
-        if (!change.after.exists)
-            return Promise.resolve("Deleted");
-
-        const receiverRefAfter: admin.firestore.DocumentReference = change.after.get("receiver_match");
-        const receiverRefBefore: admin.firestore.DocumentReference = change.before.get("receiver_match");
-        if (receiverRefAfter !== receiverRefBefore) {
-            receiverRefAfter.update({
-                donor_matches: admin.firestore.FieldValue.arrayUnion(change.after.ref)
+        const contributorRefAfter: DocumentReference = change.after.get("contributor_match");
+        const contributorRefBefore: DocumentReference = change.before.get("contributor_match");
+        if (contributorRefAfter  && contributorRefAfter !== contributorRefBefore || shouldUpdateStatus) {
+            contributorRefAfter.update({
+                status: change.after.get("status"),
+                receiver_match: change.after.ref
             }).catch(console.error);
-            console.log("Added donor match to", receiverRefAfter);
+            console.log("Updated contributor", contributorRefAfter);
+        }
+
+        const donorsRefsAfter: DocumentReference[] = change.after.get("donor_matches");
+        const donorsRefsBefore: DocumentReference[] = change.before.get("donor_matches");
+        if (donorsRefsAfter && donorsRefsAfter !== donorsRefsBefore || shouldUpdateStatus) {
+            donorsRefsAfter.forEach((donorRef) => donorRef.update(
+                {
+                    status: change.after.get("status"),
+                    receiver_match: change.after.ref
+                }
+            ).catch(console.error));
+            console.log("Updated donors", donorsRefsAfter);
         }
 
         return Promise.resolve("Updated references");
